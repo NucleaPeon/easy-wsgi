fa36b2d6f061ad74125e95884954849ae2e92cf7
Daniel Kettle <initial.dann@gmail.com>
2014-05-22 03:07:55 -0700

    Updated README.md and tweaked config.py

--- README.md
+++ README.md
@@ -1,18 +1,68 @@
 easy-wsgi
-----
+========
 
 WSGI mini-framework that is easy to set up and configure to serve
 dynamic content in html pages.
 
-PROJECT IS BROKEN CURRENTLY!
-wsgi_parser needs work before it can render webpages properly.
-Documentation and Installation docs need to be written
-Basic examples need to be written
+TODO
+========
 
-Debugging
+* wsgi_parser needs work before it can render webpages properly.
+* Documentation and Installation docs need to be written
+* easy-wsgi.py needs work
+* Safe eval() capabilities for arguments
+* Tags read by regex
+
+Examples
+========
+
+Templating
 ------
 
-If you get an Internal Server Error, you can find an entry in the
-/var/log/apache2/error.log file when using apache2 to host wsgi.
+Whether to parse `<?wsgi-template ?>` tags, pointing to a python file with 
+required methods
+
+Format:
+
+`<? [(optional)variablename]=path.to.module.class_or_method(args, kwargs) ?>`
+
+The format is similar to a basic python statement. easy-wsgi allows a user to
+assign the results of a method or class into a variable name. 
+
+    * `name=statement`: there can be no spaces between your variable name, equals sign and the statement
+    
+The path to the module is separated by periods ".", although to use python build-ins,
+you won't be using any modules.
+
+    * <? print("hello world") ?> <!-- no modules -->
+    * <? my.module.method.print("hello world") ?> <!-- my module -->
+    * <? length=len([1, 4, 5, 6, 7]) ?>
+
+At the end of the module path (if applicable, otherwise the statement), you can
+have a class or a method, or both.
+
+    * class: <? obj=my.module.method.MyClass() ?>
+      <? obj.method() ?>
+    * method: <? m=my.module.do_something() ?>
+    * both: <? m=my.module.method.MyClass().method() ?> <!-- this works best in singleton patterns or one-offs -->
+    * also: <? m=my.module.do_something ?> <!-- stores method in "m" variable -->
+    
+In a future release it may be possible to have complex arguments in methods 
+and classes (dict, list), but for now we limit it to int/float/string/bool.
+
+Master Template Example
+------
+
+If you want every single <title> tag to be generated that goes through easy-wsgi,
+use a **Master Template**. 
+
+This is done by:
+
+    * Enabling `USE_TEMPLATES = True` in config.py
+    * Associating the 'title' key in the MASTER_TEMPLATE dictionary to a method
+    * Writing a <title></title> tag into your page
+
+Each title tag should return the results of the method inside.
+(for tag modifiers themselves, please use javascript)
 
 
--- config.py
+++ config.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 '''
 :Description:
-    Configuration file (as a python module) that determines the
-    behaviour of easy-wsgi. 
+    Python Module that holds configuration options
     
 :Options:
-    - WEB_FOLDER: string path; Similar to the Directory tag of apache2, this points to the folder where wsgi contents are contained. This will be added to the PYTHONPATH variable.
-    - WEB_ENTRY_POINT: string path; The webpage (usually index.html) that you want wsgi to present to the user when they enter in the root (usually the hostname (localhost or domain name))
-    - JS: string path; Default javascript contents directory
-    - IMAGES: string path; Default images contents directory
-    - CSS: string path; Default CSS contents directory
-    - SCRIPTS: string path; Default script contents directory
-    - LOG_FILE: string path; Default logfile for state changes
-    - ERROR_FILE: string path; Default logfile for errors
+    - WEB_FOLDER: list of os.path; Paths to be added to PYTHONPATH, where modules can be located and run.
+    - WEB_ENTRY_POINT: os.path; The webpage (usually index.html) that you want wsgi to present to the user
+    - JS: os.path; Default javascript contents directory
+    - IMAGES: os.path; Default images contents directory
+    - CSS: os.path; Default CSS contents directory
+    - SCRIPTS: os.path; Default script contents directory
+    - LOG_FILE: os.path; Default logfile for state changes
+    - ERROR_FILE: os.path; Default logfile for errors
 '''
+
 import os, datetime
-WEB_FOLDER          = os.path.join(os.sep, 'var', 'www', 'default')
+
+WEB_FOLDER          = [os.path.join(os.sep, 'var', 'www')]
 WEB_ENTRY_POINT     = os.path.join(os.sep, 'var', 'www', 'index.html')
 
 # Set the PYTHONPATH variable to itself plus the WEB_FOLDER
@@ -33,24 +34,14 @@ SCRIPTS             = os.path.join(WEB_FOLDER, 'scripts')
 LOG_FILE            = os.path.join(os.sep, 'var', 'log', 'easy-wsgi.log')
 ERROR_FILE          = os.path.join(os.sep, 'var', 'log', 'easy-wsgi.err')
 
-# Whether to parse <?wsgi-template ?> tags, pointing to a python file with required methods
-# Example:
-#   <?wsgi-template title import.python.module *args ?>
-# would replace the <title> tag with import.python.module.title
-# - method is the name of the tag. *args can contain 'end_bracket' string
-#   which forces the </tag> instead of <tag>
-
+# Whether to use a Master Template on every loaded page
 USE_TEMPLATES = False
 
-# Format is {tag : path to load for tag -or- a literal string}
-# Explaination:
-#   Persistant load is a way in which a tag will always have contents
-#   appended to it regardless of which page is produced
-#   For example, always having a sidebar for navigation in the <body> tag.
+# Associate tags with python methods 
+MASTER_TEMPLATE = {'*': print}
+
 #
-#   If you want an entire file read, use: open(os.path.join(
-#       path, to, file)).read() 
-#   in the place of the string. It is a good idea to modularize your code
-#   and place small snippets or files here.
-#TEMPLATES     = {'html': '<!--\n Author: \nDate: {}\n-->'.format(str(datetime.datetime.now())),
-#                 '/html': '</html>'}
+# Example:
+#       MASTER_TEMPLATE = {'title': my.module.print_title,
+#                          'h1'   : my.biggest.heading}
+#
