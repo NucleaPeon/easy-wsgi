a2fa782ab9780a523fd3c09ea3c038c707c552e3
Daniel Kettle <initial.dann@gmail.com>
2014-05-23 22:45:32 -0700

    the groups() returned was itself in a tuple, therefore failed to function. This is fixed

--- wsgi_parser.py
+++ wsgi_parser.py
@@ -20,18 +20,41 @@ logging.basicConfig(filename='/var/log/easywsgi/parser.log', filemode='w',
                    level=logging.DEBUG)
 
 def custom_parser(statement):
-    # use regex to grab an equals sign if NOT in () enclosure
-    # If = exists to left of (), assign it a variable
-    # Continue parsing right of = or whole statement if not found
-    # Look for ()
-    # parse arguments and kwargs inside () if applicable
-    return eval(statement)
+    # Regex Description:
+    # 
+    # Expects 0 or 1 "=" signs in the string before the brackets ()
+    # The string(s) returned are:
+    #   If before the = sign (if applicable): no periods allowed
+    #   If after the = sign, periods are allowed, up until the (
+    #   Anything between the ( and ) is put into another group.
+    #
+    # Ends after the () 
+
+    pattern = re.compile('([\w^\.]*)=?([\w\.]+)*(\((.*)\)|\Z)') 
+    def parse_attributes(*args):
+        # arg[0] is either variablename or import-to-method (asdf from `asdf=datetime.datetime.now`
+        #       or datetime.datetime.now if variable name not assigned)
+        # arg[1] module and method path name reference (ex: datetime.datetime.now)
+        #       This only applies if a variablename is used. if arg[1] not None, arg[0] = arg[1]
+        # arg[2] is what is between the brackets (or empty if no brackets in case of just module/method ref)
+        #       If there is something in between the brackets, arg[3] is not None. Else this will be empty or "()"
+        # arg[3] If args/kwargs are used, this is the string.
+        
+        # In order to parse kwargs, use this regular expression: 
+        # (\w*)|\w*=*\w*|\"(.*?)" # NOT DONE, almost done
+        # (test code)
+        # "hello world", 5, kwarg="something, something, something else" 
+        #result = eval(args[0] + args[2])
+        #logging.debug("result is {}".format(args[0] + args[2]))
+        #return args[0] + args[2]
+        return args[0][0] + args[0][2]
+    
+    return eval(parse_attributes(pattern.search(statement).groups()))
 
 def parse(source):
     '''
     '''
-    logging.debug("Beginning parsing source {}".format(source))
-    pattern = re.compile('\<\?\s(.*?)\s\?\>')
+    pattern = re.compile('\<\?\s(.*?)\s\?\>') # <? ?> tags
     matches = pattern.findall(source)
     for m in matches:
         logging.debug("Found tag {}".format(m))
